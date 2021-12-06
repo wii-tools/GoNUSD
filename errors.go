bb7eac999e40b490ef61f25b760af837d3e854e3
Spotlight <spotlight@joscomputing.space>
2021-12-05 21:03:41 -0600

    Utilize existing WAD types via wadlib
    This reduces complexity and code duplication.

--- errors.go
+++ errors.go
@@ -7,13 +7,15 @@ import (
 
 var (
 	ErrInvalidTitleID                 = errors.New("invalid title ID")
-	ErrTitleNotFound                  = errors.New("title not found")
-	ErrTicketNotFound                 = errors.New("ticket not available")
 	ErrCommonKeyCipher                = errors.New("failed to create a cipher using the common key")
 	ErrTMDInvalidSignatureTypeFailure = errors.New("GoNUSD only supports signature type RSA 2048") // TODO: other consoles might use a different signature bit length, make sure this gets support down the line (just read signature type, then the signature and then the rest)
 
-	ErrContentNotFound = func(numberOfContents uint16, id uint32) error {
-		return fmt.Errorf("number of contents is %d, but content %08x is not available", numberOfContents, id)
+	ErrFileNotFound = func(filename string) error {
+		return fmt.Errorf("file %s was not found on NUS", filename)
+	}
+
+	ErrServerError = func(filename string, error int) error {
+		return fmt.Errorf("server returned %d upon download of %s", error, filename)
 	}
 
 	ErrTicketDecryptionFailure = func(err error) error {
--- nusd.go
+++ nusd.go
@@ -1,12 +1,8 @@
 package GoNUSD
 
 import (
-	"bytes"
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/sha1"
-	"encoding/binary"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/wii-tools/wadlib"
@@ -14,150 +10,82 @@ import (
 
 const nusdUrl = "http://ccs.cdn.wup.shop.nintendo.net/ccs/download"
 
-func getContentIV(index uint16) []byte {
-	iv := [16]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
-	iv[0] = uint8(index >> 8)
-	iv[1] = uint8(index & 0xff)
-	return iv[:]
-}
-
-func downloadContents(tmd wadlib.BinaryTMD, records []wadlib.ContentRecord, calculateSHA1 bool, ticket *wadlib.Ticket) ([][]byte, error) {
-	buffer := make([][]byte, tmd.NumberOfContents)
-
-	if ticket != nil {
-		if err := ticket.DecryptKey(); err != nil {
-			return nil, ErrTicketDecryptionFailure(err)
-		}
-	}
-
-	var i uint16 = 0
-	for ; i < tmd.NumberOfContents; i++ {
-		contentURL := fmt.Sprintf("%s/%016x/%08x", nusdUrl, tmd.TitleID, records[i].ID)
-		contentResponse, err := http.Get(contentURL)
-
-		if err != nil {
-			return nil, ErrHTTPFailure(contentURL, err)
-		}
-
-		if contentResponse.StatusCode != http.StatusOK {
-			return nil, ErrContentNotFound(tmd.NumberOfContents, records[i].ID)
-		}
-
-		buffer[i] = make([]byte, contentResponse.ContentLength)
-		if err = binary.Read(contentResponse.Body, binary.BigEndian, buffer[i]); err != nil {
-			return nil, ErrBufferReadFailure(fmt.Sprintf("Content %08x", records[i].ID), err)
-		}
-
-		if ticket != nil {
-			block, err := aes.NewCipher(ticket.TitleKey[:])
-			if err != nil {
-				return nil, ErrCommonKeyCipher
-			}
-
-			blockMode := cipher.NewCBCDecrypter(block, getContentIV(records[i].Index))
-
-			decryptedData := make([]byte, len(buffer[i]))
-			blockMode.CryptBlocks(decryptedData, buffer[i])
-
-			if calculateSHA1 {
-				sha1sum := sha1.Sum(decryptedData)
-				if !bytes.Equal(records[i].Hash[:], sha1sum[:]) { // TODO: sometimes they're not equal, figure out why
-					return nil, ErrInvalidHash(records[i].ID)
-				}
-			}
-
-			copy(buffer[i], decryptedData)
-		}
-	}
-
-	return buffer, nil
-}
-
 // Download downloads the specified title. Requires both the high and low title ID to be concatenated.
 // Returns: TMD, Ticket (if available) plus all the contents (all raw, big endian).
 // TODO: allow downloading content for other systems (Wii U, DSi + 3DS)
-func Download(titleID uint64, version uint16, calculateSHA1, decodeAutomatically bool) ([][]byte, error) {
-	tmdURL := fmt.Sprintf("%s/%016x/tmd", nusdUrl, titleID)
+func Download(titleID uint64, version uint16, downloadTicket bool) (*wadlib.WAD, error) {
+	var tmdFilename string
 	if version != 0 {
-		tmdURL += fmt.Sprintf(".%d", version)
+		tmdFilename = fmt.Sprintf("tmd.%d", version)
+	} else {
+		tmdFilename = "tmd"
 	}
 
-	tmdResponse, err := http.Get(tmdURL)
+	var wad wadlib.WAD
+
+	tmdData, err := nusdFetch(titleID, tmdFilename)
 	if err != nil {
-		return nil, ErrHTTPFailure(tmdURL, err)
+		return nil, err
+	}
+	if wad.LoadTMD(tmdData) != nil {
+		return nil, err
 	}
 
-	if tmdResponse.StatusCode != http.StatusOK {
-		return nil, ErrTitleNotFound
-	}
+	// Manually download and insert all contents
+	wad.Data = make([]wadlib.WADFile, wad.TMD.NumberOfContents)
 
-	rawTMD := make([]byte, tmdResponse.ContentLength)
-	if err = binary.Read(tmdResponse.Body, binary.BigEndian, rawTMD); err != nil {
-		return nil, ErrBufferReadFailure("TMD", err)
-	}
+	var i uint16 = 0
+	for ; i < wad.TMD.NumberOfContents; i++ {
+		currentRecord := wad.TMD.Contents[i]
 
-	tmdBuffer := bytes.NewBuffer(rawTMD)
-	var tmd wadlib.BinaryTMD
-	if err = binary.Read(tmdBuffer, binary.BigEndian, &tmd); err != nil {
-		return nil, ErrBufferReadFailure("TMD header", err)
-	}
-
-	records := make([]wadlib.ContentRecord, tmd.NumberOfContents)
-	if err = binary.Read(tmdBuffer, binary.BigEndian, &records); err != nil {
-		return nil, ErrBufferReadFailure("TMD content", err)
-	}
-
-	if tmd.SignatureType != wadlib.SignatureRSA2048 {
-		return nil, ErrTMDInvalidSignatureTypeFailure
-	}
-
-	if !decodeAutomatically {
-		data, err := downloadContents(tmd, records, calculateSHA1, nil)
+		contentData, err := nusdFetch(titleID, fmt.Sprintf("%08x", currentRecord.ID))
 		if err != nil {
 			return nil, err
 		}
 
-		buffer := make([][]byte, 2)
-
-		copy(buffer[0], rawTMD)
-		buffer = append(buffer, data...)
-
-		return buffer, nil
+		file := wadlib.WADFile{
+			ContentRecord: currentRecord,
+			RawData:       contentData,
+		}
+		wad.Data[currentRecord.Index] = file
 	}
 
-	ticketURL := fmt.Sprintf("%s/%016x/cetk", nusdUrl, titleID)
-	ticketResponse, err := http.Get(ticketURL)
-	if err != nil {
-		return nil, ErrHTTPFailure(ticketURL, err)
+	if !downloadTicket {
+		// We're all done.
+		return &wad, nil
 	}
 
-	if ticketResponse.StatusCode != http.StatusOK {
-		return nil, ErrTicketNotFound
-	}
-
-	rawTicket := make([]byte, ticketResponse.ContentLength)
-	if err = binary.Read(ticketResponse.Body, binary.BigEndian, rawTicket); err != nil {
-		return nil, ErrBufferReadFailure("Ticket", err)
-	}
-
-	var (
-		ticketBuffer = bytes.NewBuffer(rawTicket)
-		ticket       wadlib.Ticket
-	)
-	if err = binary.Read(ticketBuffer, binary.BigEndian, &ticket); err != nil {
-		return nil, ErrBufferReadFailure("Ticket contents", err)
-	}
-
-	data, err := downloadContents(tmd, records, calculateSHA1, &ticket)
+	ticketData, err := nusdFetch(titleID, "cetk")
 	if err != nil {
 		return nil, err
 	}
+	if err = wad.LoadTicket(ticketData); err != nil {
+		return nil, err
+	}
 
-	buffer := make([][]byte, 2)
+	return &wad, nil
+}
 
-	buffer[0] = rawTMD
-	buffer[1] = rawTicket
-	buffer = append(buffer, data...)
+func nusdFetch(titleID uint64, filename string) ([]byte, error) {
+	url := fmt.Sprintf("%s/%016x/%s", nusdUrl, titleID, filename)
 
-	return buffer, nil
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, ErrHTTPFailure(url, err)
+	}
+
+	if response.StatusCode == http.StatusNotFound {
+		return nil, ErrFileNotFound(filename)
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return nil, ErrServerError(filename, response.StatusCode)
+	}
+
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, ErrBufferReadFailure(filename, err)
+	} else {
+		return contents, nil
+	}
 }
